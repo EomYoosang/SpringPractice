4262b9a46b0c1d16a96c328ef71eb60cb00a6b51
EomYoosang <dbtkd1801@gmail.com>
2022-04-17 20:09:48 +0900

    init

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+    @Id
+//    @Column(name = "id", nullable = false)
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+}
