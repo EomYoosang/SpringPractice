632f3a5b727bd0c1e6c1b916263326ce5aed412f
EomYoosang <dbtkd1801@gmail.com>
2022-05-02 01:18:18 +0900

    test: add MemberRepositoryTest

--- src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -16,14 +16,20 @@ import javax.persistence.EntityManager;
 @SpringBootTest
 public class MemberRepositoryTest {
     @Autowired MemberRepository memberRepository;
+
     @Test
     @Transactional
     @Rollback(false)
-    public void testMember() {
+    public void testMember() throws Exception {
+        // given
         Member member = new Member();
         member.setUsername("memberA");
+
+        // when
         Long savedId = memberRepository.save(member);
         Member findMember = memberRepository.find(savedId);
+
+        // then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
 
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
